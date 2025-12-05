48f7892ff67f8607384638736fb30ae00db9349d
AlejoGDOfficial <alejogdoff@gmail.com>
2025-12-05 09:55:09 -0500

    positionSafety && borderless ALETab

--- source/ale/ui/ALETab.hx
+++ source/ale/ui/ALETab.hx
@@ -23,6 +23,8 @@ class ALETab extends ALEUISpriteGroup
         return draggable;
     }
 
+    final positionSafety:Bool;
+
     var dragging(default, set):Bool;
     function set_dragging(val:Bool):Bool
     {
@@ -33,7 +35,7 @@ class ALETab extends ALEUISpriteGroup
             var mousePos:FlxPoint = FlxG.mouse.getScreenPosition(cameras[0]);
 
             mouseOffset = FlxPoint.get(mousePos.x - this.x, mousePos.y - this.y);
-        } else {
+        } else if (positionSafety) {
             x = FlxMath.bound(x, -width + ALEUIUtils.OBJECT_SIZE, FlxG.width - ALEUIUtils.OBJECT_SIZE);
             y = FlxMath.bound(y, ALEUIUtils.OBJECT_SIZE, FlxG.height);
         }
@@ -43,36 +45,45 @@ class ALETab extends ALEUISpriteGroup
 
     var mouseOffset:FlxPoint;
 
-    public function new(?x:Float, ?y:Float, ?w:Float, ?h:Float, ?title:String, ?isDraggable:Bool)
+    public function new(?x:Float, ?y:Float, ?w:Float, ?h:Float, ?title:String, ?isDraggable:Bool, ?borderless:Bool, ?positionSafety:Bool)
     {
         super(x, y);
 
         w ??= (ALEUIUtils.OBJECT_SIZE * 10);
         h ??= (ALEUIUtils.OBJECT_SIZE * 10);
 
-        border = new ALEButton(0, 0, title ?? 'Title', w, null, null, false);
-        border.label.alignment = 'left';
-        border.label.x = 10;
-        border.changeCursorSkin = false;
-        add(border);
-        border.y = this.y - border.bg.height;
-        border.pressCallback = () -> {
-            if (draggable)
-                dragging = true;
-        };
-        border.releaseCallback = () -> {
-            if (draggable)
-                dragging = false;
-        };
-
         bg = new ALEUISprite();
 		bg.pixels = ALEUIUtils.uiBitmap(Math.floor(w), Math.floor(h), false, -75);
 		bg.updateHitbox();
 		add(bg);
 
+        borderless ??= false;
+
+        positionSafety ??= true;
+
+        this.positionSafety = positionSafety && !borderless;
+        
+        if (!borderless)
+        {
+            border = new ALEButton(0, 0, title ?? 'Title', w, null, null, false);
+            border.label.alignment = 'left';
+            border.label.x = 10;
+            border.changeCursorSkin = false;
+            add(border);
+            border.y = this.y - border.bg.height;
+            border.pressCallback = () -> {
+                if (draggable)
+                    dragging = true;
+            };
+            border.releaseCallback = () -> {
+                if (draggable)
+                    dragging = false;
+            };
+        }
+
         dragging = false;
 
-        draggable = isDraggable ?? true;
+        draggable = isDraggable ?? true && !borderless;
     }
 
     override function uiUpdate(elapsed:Float)
